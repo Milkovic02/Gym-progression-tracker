6ee8002bcfad54683ea413d1fdd85baf1d3ce9df
Milkovic02 <106239934+Milkovic02@users.noreply.github.com>
2022-07-13 20:01:35 +0200

    Update header.h

--- header.h
+++ header.h
@@ -2,20 +2,22 @@
 #define HEADER_H
 
 typedef struct vjezbe {
+	int id;
 	char imeVjezbe[45];
 	int sets;
 	int reps;
 }VJEZBE;
 
-int glavniIzbornik();
+int izbornik();
 void kreiranjeDatoteke();
 void dodajVjezbu();
 void* ucitavanjeVjezbi();
 void ispisivanjeVjezbi(const VJEZBE* const);
 void* pretrazivanjeVjezbi(VJEZBE* const);
-void brisanjeVjezbe(VJEZBE** const trazeniStudent, const VJEZBE* const poljeStudenata);
-void brisanjeDatoteke();
+void brisanjeVjezbe(VJEZBE* poljeVjezbi);
 int izlazIzPrograma(VJEZBE*);
+int bmibmrCalc();
+
 
 #endif //HEADER_H
 
